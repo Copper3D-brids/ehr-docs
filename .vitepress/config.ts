d573be9ea48b5689d280bf8b274b4c1b005290c9
skycoco <skycoco1994@gmail.com>
2024-03-04 10:22:37 +1300

    update road map and fhir profiling guide

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -178,6 +178,10 @@ function createEHRColumnSidebar() {
                   text: "mCODE Implementation Guide",
                   link: "/docs/ehr/fhir/fhir-ig/02-fhir-mcode",
                 },
+                {
+                  text: "FHIR Profiling Training",
+                  link: "/docs/ehr/fhir/fhir-ig/03-fhir-profiling",
+                },
               ]
             },
             {
